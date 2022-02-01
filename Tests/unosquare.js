b8fa43e5e08a2f1e257171fcbe81d32aae272afa
Athziri Rodriguez <athziri.rodriguez@unosquare.com>
2022-02-01 12:11:31 -0600

    Asserts

--- Tests/unosquare.js
+++ Tests/unosquare.js
@@ -1,9 +1,20 @@
 module.exports = {
-    'Demo test unosquare' : function(browser) {
+    'Demo test unosquare 1' : function(browser) {
       browser
       .windowMaximize()
       .url('https://www.unosquare.com')
       .waitForElementVisible('body')
       .end();
-    }
-  };
+    },
+    'Demo test unosquare 2' : function(browser) {
+        browser
+        .windowMaximize()
+        .url('https://www.unosquare.com')
+        .waitForElementVisible('body')
+        .verify.visible('xxxx')
+        .url('https://www.google.com')
+        .assert.visible('.non_existing')
+        .url('https://www.amazon.com')
+        .end();
+    },
+};
