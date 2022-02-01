2ee787f387ead25393ec1ceedb7c603f47d069ea
Athziri Rodriguez <athziri.rodriguez@unosquare.com>
2022-02-01 15:10:32 -0600

    Basic Test

--- Tests/unosquare.js
+++ Tests/unosquare.js
@@ -11,9 +11,9 @@ module.exports = {
         .windowMaximize()
         .url('https://www.unosquare.com')
         .waitForElementVisible('body')
-        .verify.visible('xxxx')
+        .assert.not.visible('xxxx')
         .url('https://www.google.com')
-        .assert.visible('.non_existing')
+        .assert.not.visible('.non_existing')
         .url('https://www.amazon.com')
         .end();
     },
@@ -35,9 +35,9 @@ module.exports = {
         .assert.attributeContains("li a[href = '/Services']", 'class', 'nav')
         .assert.attributeEquals("li a[href = '/Services']", 'class', 'nav-link')
         .end();
-      },
+    },
 
-      'Demo test unosquare 5' : ''+ function(browser) {
+    'Demo test unosquare 5' : ''+ function(browser) {
         browser
         .windowMaximize()
         .url('https://www.unosquare.com')
@@ -47,9 +47,9 @@ module.exports = {
         .waitForElementVisible("li a[href = '/Services']")
         .assert.containsText("li a[href = '/Services']", "SERVICES"); //This is case Sensitive
         browser.end();
-      },
+    },
 
-      'Demo test unosquare 6' : function(browser) {
+    'Demo test unosquare 6' : ''+ function(browser) {
         browser
         .windowMaximize()
         .url('https://www.unosquare.com')
@@ -61,5 +61,32 @@ module.exports = {
         .assert.cssProperty("li a[href = '/About']", 'display', 'block')
         .assert.not.cssProperty("li a[href = '/About']", 'font-size', '12px');
         browser.end();
-      }
+    },
+      
+    'Demo test unosquare Pull Request' : function(browser) {
+        browser
+        .windowMaximize()
+        .url('https://www.unosquare.com')
+        .waitForElementVisible('body')
+
+        // assert[.not].visible()
+        .assert.visible('.card.h-100')
+        .assert.not.visible("li a[href = '/Industries/LifeSciences']")
+
+        // assert[.not].title() 
+        //DEPRECATED: the assertion .title() has been deprecated and will be removed from future versions. 
+        //Using assert.titleEquals()
+        .assert.titleEquals("Digital Transformation Services | Agile Staffing Solutions | Unosquare")
+        .assert.not.titleEquals("Nightwatch.js")
+
+        //assert[.not].urlContains()        
+        .assert.urlContains('unosquare.com/')
+        .assert.not.urlContains('/ContactUs')
+
+        //assert[.not].urlEquals()
+        .assert.urlEquals('https://www.unosquare.com/')
+        .assert.not.urlEquals('Nightwatch.js')
+    },
+    
+     
 };
